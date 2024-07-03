b6542c83ac3ce53b313a155e5f9ccad7c5b516ae
Alina Li <alinali87@gmail.com>
2024-07-03 22:38:35 +0400

    refact: brain-even game

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,67 +1,34 @@
 import prompt
 import random
 from brain_games.cli import welcome_user
-from brain_games.utils import congrat_user
+from brain_games.utils import congrat_user, game_over
 
 
-# TODO: refactor
+ROUNDS_TO_WIN = 3
+
+
+def generate_question():
+    num = random.randint(1, 100)
+    correct_answer = "yes" if num % 2 == 0 else "no"
+    return num, correct_answer
+
+
 def even_game():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct = 0
-    while True:
-        num = random.randint(1, 100)
-        correct_answer = "yes" if num % 2 == 0 else "no"
+    while correct < ROUNDS_TO_WIN:
+        num, correct_answer = generate_question()
         print(f"Question: {num}")
-        a = prompt.string("Your answer: ")
-        if a == correct_answer:
+        answer = prompt.string("Your answer: ")
+        if answer == correct_answer:
             print("Correct!")
             correct += 1
         else:
-            print(f"'{a}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return False
-        if correct >= 3:
-            congrat_user(name)
-            return True
-
-
-# TODO: clean
-def play_game(gen_question):
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    # start_game(start_text)
-    counter = 0
-    while counter < 3:
-        question, correct_answer = gen_question()
-        print(f"Question: {question}")
-        answer = input('Your answer: ')
-        if answer == correct_answer:
-            print('Correct!')
-            counter += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}")
-            # if answer != correct_answer:
-            #     print(f"'{answer}' is wrong answer ;(. "
-            #           f"Correct answer was '{correct_answer}'.")
-            # else:
-            # print(f"'{answer}' is wrong answer ;(. "
-            #       f"Correct answer was '{correct_answer}'.")
-            # raise NotImplementedError("This 'if' branch is impossible")
-            # print(f"Let's try again, {name}!")
-            return False
+            game_over(name, answer, correct_answer)
+            return
     congrat_user(name)
-    # congratulate(name)
-    return True
-
-
-def main():
-    even_game()
-    # play_game(gen_question_brain_even)
 
 
 if __name__ == '__main__':
-    main()
+    even_game()
--- brain_games/utils.py
+++ brain_games/utils.py
@@ -11,6 +11,19 @@ def congrat_user(name: str):
     print(f"Congratulations, {name}!")
 
 
+def game_over(name: str, answer: str, correct_answer: str):
+    """ Print the message in case of wrong answer
+
+    Args:
+        name: name of the user
+        answer: user's answer
+        correct_answer: correct answer
+    """
+    print(f"'{answer}' is wrong answer ;(. "
+          f"Correct answer was '{correct_answer}'.\n"
+          f"Let's try again, {name}!")
+
+
 def op_to_string(op: Callable) -> str:
     """ Produce the string representing an arithmetic operation
         add: "+"
